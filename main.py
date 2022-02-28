89561554686edec247bb70907e1e2cf9ffac852b
Evan Brackenbury <ebrack1@gmail.com>
2022-02-28 17:38:14 -0500

    added normalization to main function

--- main.py
+++ main.py
@@ -2,17 +2,22 @@ import spotify
 import cleanjson
 import load_raw_data
 import database_credentials
+import normalization
 
 
 # select_job = input()
 
 def main():
 
+    cnxn = database_credentials.cnxn
+
     spotify_json_data = spotify.get_spotify_data(unix='', limit=50)
 
     clean_data = cleanjson.convert_spotify_json_to_df(json_file=spotify_json_data)
 
-    load_raw_data.load_raw_data(connection=database_credentials.cnxn, dataframe=clean_data)
+    load_raw_data.load_raw_data(connection=cnxn, dataframe=clean_data)
+
+    normalization.normalize_data(connection=cnxn)
 
 
 if __name__ == '__main__':
